331a37917495cc92611be41948a597e730ad20e0
Ajay <ajaynimmala18@gmail.com>
2026-05-28 08:05:25 +0530

    Project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login';
+import { RegisterComponent } from './component/register/register';
+import { VerifyComponent } from './component/verify/verify';
+import { ResetPasswordComponent } from './component/resetpassword/resetpassword';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path :'/login',component: LoginComponent },
+  {path :'/register', component :RegisterComponent },
+  {path : 'verify' ,component :VerifyComponent },
+  {path :'/resetpassword',component:ResetPasswordComponent}
+];
